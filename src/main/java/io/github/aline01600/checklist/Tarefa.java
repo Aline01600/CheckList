a10abd4cc2c05e35045a517accaa40ffcc7ed3ff
Aline Soares <117992962+Aline01600@users.noreply.github.com>
2025-07-20 16:19:33 -0300

    Definição da classe Tarefa

--- src/main/java/io/github/aline01600/checklist/Tarefa.java
+++ src/main/java/io/github/aline01600/checklist/Tarefa.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package io.github.aline01600.checklist;
 
-/**
- *
- * @author aline
- */
 public class Tarefa {
-    
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
