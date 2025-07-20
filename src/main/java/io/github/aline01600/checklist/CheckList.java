17d91b7b10d495deabbbaa9629b7cc125c9e602f
Aline Soares <117992962+Aline01600@users.noreply.github.com>
2025-07-20 16:22:13 -0300

    Definição da classe CheckList

--- src/main/java/io/github/aline01600/checklist/CheckList.java
+++ src/main/java/io/github/aline01600/checklist/CheckList.java
@@ -1,16 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package io.github.aline01600.checklist;
 
-/**
- *
- * @author aline
- */
-public class CheckList {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class CheckList {
+    private List<Tarefa> tarefas = new ArrayList<>();
+
+    public void adicionarTarefa(String descricao) {
+        tarefas.add(new Tarefa(descricao));
+    }
+
+    public void mostrarTarefas() {
+        System.out.println("Tarefas atuais:");
+        for (Tarefa t : tarefas) {
+            System.out.println("- " + t);
+        }
+    }
+
+    public Memento criarMemento() {
+        return new Memento(new ArrayList<>(tarefas));
+    }
+
+    public void restaurarMemento(Memento memento) {
+        tarefas = new ArrayList<>(memento.getEstadoSalvo());
     }
 }
