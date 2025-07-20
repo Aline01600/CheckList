9821e492ccead792e8c786596e2ea0aea7c27e9b
Aline Soares <117992962+Aline01600@users.noreply.github.com>
2025-07-20 15:53:00 -0300

    Criação do projeto

---
+++ src/main/java/io/github/aline01600/checklist/CheckList.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.aline01600.checklist;
+
+/**
+ *
+ * @author aline
+ */
+public class CheckList {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
